9c21b407930720d7aaf66e9fe56e10d7bdec0990
TheShivamChand <32011721+TheShivamChand@users.noreply.github.com>
2018-04-08 23:55:53 +0530

    2

--- Project.c
+++ Project.c
@@ -1,7 +1,29 @@
 #include<stdio.h>
 #include<pthread.h>
 #include<unistd.h>
+
+pthread_mutex_t p;
+int initial1,initial2,final1,final2;
+
+/*void *cara
+{
+	
+}
+
+void *carb
+{
+	
+}*/
 int main()
 {
+	printf("\n\t\t\t****************************************");
+	printf("\n\t\t\t*                                      *");
+	printf("\n\t\t\t*                                      *");
+	printf("\n\t\t\t*                                      *");
+	printf("\n\t\t\t*     Traffic Intersection Problem     *");
+	printf("\n\t\t\t*                                      *");
+	printf("\n\t\t\t*                                      *");
+	printf("\n\t\t\t*                                      *");
+	printf("\n\t\t\t****************************************");
 	
 }
