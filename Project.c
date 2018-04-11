328104497c565101dfeec7ff957e47567e450468
TheShivamChand <32011721+TheShivamChand@users.noreply.github.com>
2018-04-12 00:28:11 +0530

    4

--- Project.c
+++ Project.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<pthread.h>
 #include<unistd.h>
+#include<conio.h>
 
 pthread_mutex_t p;
 int initial1,initial2,final1,final2;
@@ -14,8 +15,16 @@ void *carb
 {
 	
 }*/
+
+char intro()
+{
+	printf("Operating Systems Assignment");
+}
 int main()
 {
+	
+	intro();
+	getch();
 	printf("\n\t\t\t****************************************");
 	printf("\n\t\t\t*                                      *");
 	printf("\n\t\t\t*                                      *");
